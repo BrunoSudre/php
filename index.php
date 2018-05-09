c96aae1f5919e6a0c0b7b46e653f6573e0331ea8
Bruno Sudre <bruninhosudre@gmail.com>
2018-05-08 21:04:12 -0300

    resolvi o conflito

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-<?php
-
-echo "Hello World!";
-
-echo "Salve corolho"
-
-?>
-=======
 <?php
 
 echo "Olá Mundo!";
 echo "Olá Sudré";
 echo "Hell o";
 ?>
->>>>>>> 3eae1ef504a201dfc889bcd383e1dbcecb13190e
+
